1a7ae7e78fe09ee6627f1d2ba6589eb705fdae8b
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-08-17 14:24:05 -0400

    PR #9 changes: fix burn overrides & dummy oracle

--- src/oracle/PriceOracle.sol
+++ src/oracle/PriceOracle.sol
@@ -5,48 +5,47 @@ pragma solidity 0.8.13;
 contract PriceOracle {
 
     uint256 public eth_usd_price;
-    uint256 public share_eth_price;
+    uint256 public eth_share_price;
     uint256 public share_usd_price;
-    uint256 public eusd_eth_price;
+    uint256 public eth_eusd_price;
 
     constructor() {
-        eth_usd_price = 2000 * 10 ** 18;
-        share_eth_price = 200 * 10 ** 18;
-        share_usd_price = 10 ** 18;
-        eusd_eth_price = 2000 * 10 ** 18;
+        eth_usd_price = 2000 * 10 ** 18; // 2000 usd/eth
+        eth_share_price = 200 * 10 ** 18; // 200 SHARE/eth
+        eth_eusd_price = 2000 * 10 ** 18; // 2000 eusd/eth
+        share_usd_price = 10 ** 18; // 10 dollar/share
     }
 
     function getETHUSDPrice() public view returns(uint256){
         return eth_usd_price;
     }
 
-    function getShareETHPrice() public view returns(uint256) {
-        return share_eth_price;
+    function getETHSHAREPrice() public view returns(uint256) {
+        return eth_share_price;
     }
 
     function getShareUSDPrice() public view returns(uint256) {
         return share_usd_price;
     }
  
-    function getEUSDETHPrice() public view returns(uint256) {
-        return eusd_eth_price;
+    function getETHEUSDPrice() public view returns(uint256) {
+        return eth_eusd_price;
     }
-
 
     function setETHUSDPrice(uint256 _price) public {
         eth_usd_price = _price;
     }
 
-    function setShareETHPrice(uint256 _price) public {
-        share_eth_price = _price;
+    function setETHSharePrice(uint256 _price) public {
+        eth_share_price = _price;
     }
 
     function setShareUSDPrice(uint256 _price) public {
         share_usd_price = _price;
     }
 
-    function setEUSDETHPrice(uint256 _price) public {
-        eusd_eth_price = _price;
+    function setETHEUSDPrice(uint256 _price) public {
+        eth_eusd_price = _price;
     }
 
 }
