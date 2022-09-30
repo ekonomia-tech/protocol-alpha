850bed095e57c757b099096d3ba0246fa39c385f
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-09-29 20:15:25 -0400

    2nd PR #44 changes: reformat & mark TODO for tests

--- src/interfaces/IPHOOracle.sol
+++ src/interfaces/IPHOOracle.sol
@@ -6,27 +6,14 @@ pragma solidity ^0.8.13;
 /// @author Ekonomia: https://github.com/Ekonomia
 interface IPHOOracle {
     event PriceUpdated(uint256 indexed latestPHOUSDPrice, uint256 indexed blockTimestampLast);
-    event PriceThresholdExceeded(bool priceThresholdChangeExceeded);
-    event PriceSourceUpdated(address indexed priceSource);
+    // event PriceThresholdExceeded(bool priceThresholdChangeExceeded);
     event PriceUpdateThresholdChanged(uint256 priceUpdateThreshold);
 
     /// @notice gets latest PHO/USD price as per internal oracle
     /// @return price USD/PHO to 18 decimals
-    /// TODO - see note in PHOTWAPOracle for this function regarding return value type
-    function getPrice() external returns (uint256);
+    function updatePrice() external returns (uint256);
 
-    /// @notice calculates return amount of inputToken with last recorded PHO price
-    /// @dev varies in the units that the return value is in based on type of oracle (ex. TWAP vs PriceFeed)
-    /// @param _token input ERC20 address
-    /// @param _amountIn total value of input token being priced
-    /// @return amountOut amount of inputToken (scaled by d18 in other pair token, based on current price
-    function consult(address _token, uint256 _amountIn) external returns (uint256);
-
-    /// @notice set the price source address that this oracle interacts with
-    /// @param _priceSource address for price source
-    function setPriceSource(address _priceSource) external;
-
-    /// @notice sets the suggested price update threshold
+    /// @notice sets the max amount the new price can differ from the last price, called the priceUpdateThreshold
     /// @param _priceUpdateThreshold the suggested price update threshold, expressed in basis points - 10 ** 6 BP corresponding to 100%
     function setPriceUpdateThreshold(uint256 _priceUpdateThreshold) external;
 }
