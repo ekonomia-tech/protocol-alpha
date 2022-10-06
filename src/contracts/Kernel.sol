45844da8204f92ccd7ab4917356449423f76691e
Niv Mimran <nmimran99@gmail.com>
2022-10-06 12:18:29 -0500

    Write Kernel + IKernel

--- src/contracts/Kernel.sol
+++ src/contracts/Kernel.sol
@@ -1,74 +1,100 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import "../interfaces/IKernel.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/IPHO.sol";
+import "../interfaces/IDispatcher.sol";
 
-contract Kernel is IKernel, Ownable {
-
-    error zeroAddressDetected();
-    error zeroValueDetected();
-    error calledNotApproved(address caller);
-    error callerAlreadyApproved();
-    error callerCeilingReached(uint256 ceiling, uint256 currentBalance, uint256 attemptedAmount);
-    error newCeilingTooLow(uint256 currentCeiling, uint256 ceilingRequested);
-
+contract Kernel is IKernel {
     IPHO public pho;
 
-    /// whitelisted contracts that can ask the Teller to mint
-    mapping(address => uint256) public mintingWhitelist;
+    uint256 PHOCeiling;
+    uint256 totalPHOMinted;
+    uint256 moduleManagerDelay = 4 weeks;
+    uint256 dispatcherDelay = 4 weeks;
+    uint256 moduleDelay = 2 weeks;
 
-    /// tracks max $PHO that whitelisted accounts can ask for
-    mapping(address => uint256) public mintingBalances;
+    IDispatcher dispatcher;
+    address moduleManager;
+    address TONGovernance;
 
-    constructor(address _phoAddress) {
-        pho = IPHO(_phoAddress);
+    modifier onlyModuleManager() {
+        if (msg.sender != moduleManager) revert Unauthorized_NotModuleManager(msg.sender);
+        _;
     }
 
-    /// @notice minting function for $PHO. only accessible to approved addresses
-    /// @param to the user to mint $PHO to
-    /// @param amount the amount of $PHO to mint
-    function mintPHO(address to, uint256 amount) external {
-        require(to != address(0), "Teller: zero address detected");
-        require(mintingWhitelist[msg.sender] > 0, "Teller: caller is not approved");
-        require(
-            mintingBalances[msg.sender] + amount <= mintingWhitelist[msg.sender],
-            "Teller: caller ceiling reached"
-        );
-        mintingBalances[msg.sender] += amount;
+    modifier onlyTONGovernance() {
+        if (msg.sender != TONGovernance) revert Unauthorized_NotTONGovernance(msg.sender);
+        _;
+    }
+
+    constructor(
+        address _pho,
+        address _moduleManager,
+        address _dispatcher,
+        address _TONGovernance,
+        uint256 _PHOCeiling
+    ) {
+        pho = IPHO(_pho);
+        moduleManager = _moduleManager;
+        dispatcher = IDispatcher(_dispatcher);
+        TONGovernance = _TONGovernance;
+        PHOCeiling = _PHOCeiling;
+    }
+
+    function mintPHO(address to, uint256 amount) external onlyModuleManager {
+        if (to == address(0)) revert ZeroAddressDetected();
+        if (amount == 0) revert ZeroValueDetected();
+        if (totalPHOMinted + amount > PHOCeiling) {
+            revert MintingCeilingReached(PHOCeiling, totalPHOMinted, amount);
+        }
+        totalPHOMinted += amount;
         pho.mint(to, amount);
     }
 
-    /// @notice function to approve addresses of minting rights
-    /// @param caller the requesting address to be approved minting rights
-    /// @param ceiling the minting ceiling for the caller
-    function whitelistCaller(address caller, uint256 ceiling) external onlyOwner {
-        require(caller != address(0), "Teller: zero address detected");
-        require(ceiling > 0, "Teller: zero value detected");
-        require(mintingWhitelist[caller] == 0, "Teller: caller is already approved");
-        mintingWhitelist[caller] = ceiling;
-        emit CallerWhitelisted(caller, ceiling);
+    function burnPHO(address from, uint256 amount) external onlyModuleManager {
+        if (from == address(0)) revert ZeroAddressDetected();
+        if (amount == 0) revert ZeroValueDetected();
+        totalPHOMinted -= amount;
+        pho.burnFrom(from, amount);
     }
 
-    // TODO - We need to hook up the revoking and deleting to the totalPHOMinted. And start decrementing the totalPHOMinted.
-    /// @notice function to revoke addresses of minting rights
-    /// @param caller the address to be revoked minting rights
-    function revokeCaller(address caller) external onlyOwner {
-        require(caller != address(0), "Teller: zero address detected");
-        require(mintingWhitelist[caller] > 0, "Teller: caller is not approved");
-        delete mintingWhitelist[caller];
-        delete mintingBalances[caller];
-        emit CallerRevoked(caller);
+    function setPHOCeiling(uint256 newCeiling) external onlyTONGovernance {
+        if (newCeiling < totalPHOMinted) {
+            revert CeilingLowerThanTotalMinted(totalPHOMinted, PHOCeiling);
+        }
+        PHOCeiling = newCeiling;
+        emit PHOCeilingUpdated(newCeiling);
     }
 
-    /// @notice modify the caller's minting ceiling
-    /// @param caller the address to modify it's ceiling
-    /// @param newCeiling the new minting ceiling for the caller
-    function modifyCallerCeiling(address caller, uint256 newCeiling) external onlyOwner {
-        require(caller != address(0), "Teller: zero address detected");
-        require(mintingWhitelist[caller] > 0, "Teller: caller is not approved");
-        require(mintingBalances[caller] < newCeiling, "Teller: new ceiling too low");
-        mintingWhitelist[caller] = newCeiling;
-        emit CallerCeilingModified(caller, newCeiling);
+    function updateModuleManagerDelay(uint256 newDelay) external onlyTONGovernance {
+        if (newDelay == 0) revert ZeroValueDetected();
+        moduleManagerDelay = newDelay;
+        emit ModuleManagerDelayUpdated(newDelay);
+    }
+
+    function updateDispatcherDelay(uint256 newDelay) external onlyTONGovernance {
+        if (newDelay == 0) revert ZeroValueDetected();
+        dispatcherDelay = newDelay;
+        emit DispatcherDelayUpdated(newDelay);
+    }
+
+    function addModuleDelay(uint256 newDelay) external onlyTONGovernance {
+        if (newDelay == 0) revert ZeroValueDetected();
+        moduleDelay = newDelay;
+        emit ModuleDelayUpdated(newDelay);
+    }
+
+    function updateDispatcher(address newDispatcher) external onlyTONGovernance {
+        if (newDispatcher == address(0)) revert ZeroAddressDetected();
+        if (newDispatcher == address(dispatcher)) revert SameAddressDetected();
+        dispatcher = IDispatcher(newDispatcher);
+        emit DispatcherUpdated(newDispatcher);
+    }
+
+    function updateModuleManager(address newModuleManager) external onlyTONGovernance {
+        if (newModuleManager == address(0)) revert ZeroAddressDetected();
+        if (newModuleManager == moduleManager) revert SameAddressDetected();
+        moduleManager = newModuleManager;
+        emit ModuleManagerUpdated(newModuleManager);
     }
 }
--- src/interfaces/IKernel.sol
+++ src/interfaces/IKernel.sol
@@ -3,12 +3,28 @@
 pragma solidity ^0.8.13;
 
 interface IKernel {
-    event CallerWhitelisted(address indexed caller, uint256 ceiling);
-    event CallerRevoked(address indexed caller);
-    event CallerCeilingModified(address indexed caller, uint256 newCeiling);
+    error ZeroAddressDetected();
+    error ZeroValueDetected();
+    error SameAddressDetected();
+    error SameValueDetected();
+    error MintingCeilingReached(uint256 ceiling, uint256 totalPHOMinted, uint256 attemptedMint);
+    error Unauthorized_NotModuleManager(address caller);
+    error Unauthorized_NotTONGovernance(address caller);
+    error CeilingLowerThanTotalMinted(uint256 totalPHOMinted, uint256 attemptedNewCeiling);
 
-    function mintPHO(address to, uint256 amount) external;
-    function whitelistCaller(address caller, uint256 ceiling) external;
-    function revokeCaller(address caller) external;
-    function modifyCallerCeiling(address caller, uint256 newCeiling) external;
+    event PHOCeilingUpdated(uint256 newCeiling);
+    event ModuleManagerDelayUpdated(uint256 newDelay);
+    event DispatcherDelayUpdated(uint256 newDelay);
+    event ModuleDelayUpdated(uint256 newDelay);
+    event DispatcherUpdated(address indexed newDispatcher);
+    event ModuleManagerUpdated(address indexed newModuleManager);
+
+    function mintPHO(address to, uint256 amount) external; // onlyModuleManager
+    function burnPHO(address from, uint256 amount) external; // onlyModuleManager
+    function setPHOCeiling(uint256 newCeiling) external; // onlyTONGovernance
+    function updateModuleManagerDelay(uint256 newDelay) external; // 4 weeks. Rarely happens
+    function updateDispatcherDelay(uint256 newDelay) external; // 4 weeks. Rarely happens
+    function addModuleDelay(uint256 newDelay) external; // 2 weeks. For safe exit of angry people
+    function updateDispatcher(address newDispatcher) external; // onlyTONGovernance
+    function updateModuleManager(address newModuleManager) external; // onlyTONGovernance
 }
