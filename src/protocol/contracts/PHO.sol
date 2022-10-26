5db84d304979f1bd7861cc3a98a2ce749b57f894
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-10-26 11:09:55 -0400

    Import governorBravo from compound-protocol

--- src/protocol/contracts/PHO.sol
+++ src/protocol/contracts/PHO.sol
@@ -3,13 +3,14 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@protocol/interfaces/IPHO.sol";
 
 /// @title PHOTON protocol stablecoin
 /// @author Ekonomia: https://github.com/Ekonomia
-
-contract PHO is IPHO, ERC20Burnable, Ownable {
+// TODO - not sure if we can work with OZ extensions to achieve the same type of token type here. The question is that we would have to set up PHO && TON like so: contract PHO is IPHO, ERC20VotesComp {} <-- The issue here is that I'm not sure if we would have to have one extension only. Can we also have ERC20Burnable? It seems weird to me that OZ would design their contracts in a way that you could only have one extension. But it looks, based off of the errors I'm seeing coming up when trying to inherit both, that is the case.  If so, it looks like ERC20Burnable doesn't have a lot to it though, so we can just add those functions into the PHO && TON implementation contracts directly. 
+contract PHO is IPHO, ERC20VotesComp, ERC20Burnable, Ownable {
     address public kernel;
 
     modifier onlyKernel() {
