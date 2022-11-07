4a2205c58ac5915f49b23675406b118aef702a2b
Kevin Huo <5000881+kwhuo68@users.noreply.github.com>
2022-11-07 17:08:55 -0500

    add IModuleAMO (#74)
    * add IModuleAMO
    
    * small edits to remove unused

---
+++ src/modules/interfaces/IModuleAMO.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.13;
+
+interface IModuleAMO {
+    // Creates tracking shares for user and does external calls as needed
+    function stakeFor(address account, uint256 amount) external returns (bool);
+
+    // Withdraw amount for user
+    function withdrawFor(address account, uint256 amount) external;
+
+    // Withdraw all for user
+    function withdrawAllFor(address account) external;
+
+    // Get reward
+    function getReward(address account) external returns (bool);
+
+    // Queue new rewards
+    function queueNewRewards(uint256 rewards) external returns (bool);
+
+    // Staking token
+    function stakingToken() external view returns (address);
+
+    // Reward token
+    function rewardToken() external view returns (address);
+
+    // Tracks earned amount per user
+    function earned(address account) external view returns (uint256);
+}
